8b50ca579a7102069f67c4e898dcdfa3e71278d3
dest3668 <dennis.sterle@study.hs-duesseldorf.de>
2025-01-21 17:19:32 +0100

    hero implemented and styled

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <main className="container"></main>
+      <main className="container">
+        <Hero />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import smallPNG from "../assets/images/hero/small.png";
+import smallJPG from "../assets/images/hero/small.jpg";
+import mediumPNG from "../assets/images/hero/medium.png";
+import mediumJPG from "../assets/images/hero/medium.jpg";
+import largePNG from "../assets/images/hero/large.png";
+import largeJPG from "../assets/images/hero/large.jpg";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <picture>
+        <source srcSet={smallPNG} type="image/png" media="(max-width: 320px)" />
+        <source
+          srcSet={smallJPG}
+          type="image/jpeg"
+          media="(max-width: 320px)"
+        />
+
+        <source
+          srcSet={mediumPNG}
+          type="image/png"
+          media="(max-width: 1280px)"
+        />
+        <source
+          srcSet={mediumJPG}
+          type="image/jpeg"
+          media="(max-width: 1280px)"
+        />
+
+        <source srcSet={largeJPG} type="image/png" />
+        <source srcSet={largeJPG} type="image/jpeg" />
+
+        <img
+          src="@/images/hero/3690x1083.jpg"
+          alt="Hero"
+          className="hero__image"
+        />
+      </picture>
+    </div>
+  );
+};
+
+export default Hero;
